3915d537a2ecf4de9a8ae7a8e93b7434ddd82090
Kerolos Raed <keroraed123@gmail.com>
2026-05-23 15:58:14 +0300

    Enhance About page with new layout, content, and styling for improved user experience

--- VISSTA.Web/Controllers/HomeController.cs
+++ VISSTA.Web/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ public sealed class HomeController(IMediator mediator) : Controller
         return View(new HomeViewModel(featured, collection.Take(4).ToList()));
     }
 
+    [HttpGet("/About")]
     public IActionResult About() => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
